06360e44b82be7c0d21408595a835397145bd9ad
hassan <hassanak509@gmail.com>
2025-03-03 19:06:33 +0200

    create the seed.sql file that used to make the seeds

---
+++ wallet-server/database/seed.sql
@@ -0,0 +1,11 @@
+use digital_wallet_db;
+
+SOURCE database/seeds/users_seed.sql;
+SOURCE database/seeds/admins_seed.sql;
+SOURCE database/seeds/deposits_seed.sql;
+SOURCE database/seeds/transfers_seed.sql;
+SOURCE database/seeds/wallets_seed.sql;
+SOURCE database/seeds/withdraws_seed.sql;
+
+
+
