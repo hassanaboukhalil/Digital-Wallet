0e1651ed4b91a7d69723493d5affa0fb1276e68c
hassan <hassanak509@gmail.com>
2025-03-03 19:12:37 +0200

    update the wallets.sql migration

--- wallet-server/database/migrations/wallets.sql
+++ wallet-server/database/migrations/wallets.sql
@@ -3,7 +3,7 @@ create table wallets (
     user_id int(11) not null,
     wallet_name varchar(255) not null,
     balance varchar(255) not null,
-    api_key int(11) not null,
+    api_key varchar(255) not null,
     created_at timestamp not null,
     foreign key (user_id) references users(id)
 )
