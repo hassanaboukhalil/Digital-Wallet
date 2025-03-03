e3aa3e1cb70748b0e29a3f2bd3ffb027258dac4f
hassan <hassanak509@gmail.com>
2025-03-03 19:04:31 +0200

    create the admins seed

---
+++ wallet-server/database/seeds/admins_seed.sql
@@ -0,0 +1,3 @@
+insert into admins (first_name, last_name, email, pass, created_at) values
+('Hussein', 'Yehya', 'hussein@gmail.com', 'abcdefg', "2024-12-10 17:56:47"),
+("Ghassan", 'Taha', 'ghassan@gmail.com', '123456', "2024-12-11 17:56:47");
