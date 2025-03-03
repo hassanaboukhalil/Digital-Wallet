af6c9fa5d0257d3f4f904c674ed6c40e9f5a2e42
hassan <hassanak509@gmail.com>
2025-03-03 19:05:18 +0200

    create the wallets seed

---
+++ wallet-server/database/seeds/wallets_seed.sql
@@ -0,0 +1,5 @@
+insert into wallets (user_id, wallet_name, balance, api_key, created_at) values
+('1', 'main', '2400', 'aassddffgg234user1', "2024-12-18 17:56:47"),
+('2', 'main', '100', 'aassddffgg234user2', "2024-12-19 17:56:47"),
+('3', 'main', '200', 'aassddffgg234user3', "2024-12-20 17:56:47");
+
