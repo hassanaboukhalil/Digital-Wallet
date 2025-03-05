609309387c656c6c74ab8697d0cfb994cadb8d4a
hassan <hassanak509@gmail.com>
2025-03-05 07:55:30 +0200

    created the login feature

---
+++ wallet-client/js/login.js
@@ -0,0 +1,26 @@
+const email = document.getElementById("email");
+const pass = document.getElementById("pass");
+
+
+
+async function login() {
+    const form = new FormData();
+
+    form.append("email", email.value);
+    form.append("pass", pass.value);
+
+
+    const response = await axios.post(
+        "http://localhost:8080/digital_wallet/wallet-server/user/v1/login.php",
+        form
+    );
+
+    console.log(response.data)
+    if (response.data[0].message == 'success') {
+        localStorage.setItem("id", response.data[0].user.id);
+        localStorage.setItem("first_name", response.data[0].user.first_name);
+        window.location.href = "./home.html";
+    } else {
+        alert("Login failed: " + response.data[0].message);
+    }
+}
